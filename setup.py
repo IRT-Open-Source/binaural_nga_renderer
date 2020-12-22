c7ccd8f48b8ee6482901c0ffc07f59c513975e45
Michael Meier <meier@irt.de>
2020-12-22 15:05:24 +0100

    add a first integration test
    it's not much, but it's honest work ...

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ setup(
     description='Binaural NGA Renderer',
     version='1.0.0',
     license='MIT',
-    long_description=open('README.md').read() + '\n' + open('CHANGELOG.md').read(),
+    long_description=open('README.md').read() + '\n' +
+    open('CHANGELOG.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
-        'numpy~=1.14',
-        'pydub>=0.23.1',
-        'scipy~=1.0',
-        'ear~=2.0.0',
-        'h5py==2.10.0'
+        'numpy~=1.14', 'pydub>=0.23.1', 'scipy~=1.0', 'ear~=2.0.0',
+        'h5py>=3.1.0'
     ],
+    extras_require={'test': [
+        'pytest',
+        'pytest-datafiles',
+        'pytest-cov',
+    ]},
     packages=find_packages(),
     package_data={
-         "nga_binaural": ["data/*"],
+        "nga_binaural": ["data/*"],
     },
     entry_points={
         'console_scripts': [
